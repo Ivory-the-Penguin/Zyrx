e175cc6ca48f8856eb429345735dce318bf31b17
Ivory <ivory-the-penguin@proton.me>
2026-09-14 19:13:56 +0100

    Split into separate files

---
+++ src/core/list.h
@@ -0,0 +1,65 @@
+#include <assert.h>
+#include <stdint.h>
+
+#define DEFINE_ARRAY(TYPE)                                                     \
+  typedef struct {                                                             \
+    TYPE *data;                                                                \
+    uint64_t length;                                                           \
+    uint64_t capacity;                                                         \
+  } list_##TYPE##_t;                                                           \
+                                                                               \
+  void list_##TYPE##_set_capacity(list_##TYPE##_t *list, uint64_t capacity);   \
+  void list_##TYPE##_push(list_##TYPE##_t *list, TYPE item);                   \
+  void list_##TYPE##_free(list_##TYPE##_t *list);                              \
+  void list_##TYPE##_pop(list_##TYPE##_t *list);                               \
+                                                                               \
+  static inline TYPE *list_##TYPE##_at(list_##TYPE##_t *list,                  \
+                                       uint64_t index) {                       \
+    assert(index < list->length);                                              \
+    return &list->data[index];                                                 \
+  }
+
+#define IMPL_ARRAY(TYPE)                                                       \
+  void list_##TYPE##_set_capacity(list_##TYPE##_t *list, uint64_t capacity) {  \
+    assert(list->length <= capacity &&                                         \
+           "Capacity is smaller than the list length!");                       \
+                                                                               \
+    list->data = (TYPE *)realloc(list->data, capacity * sizeof(TYPE));         \
+    assert(list->data != nullptr && capacity != 0 && "Allocation failed!");    \
+                                                                               \
+    list->capacity = capacity;                                                 \
+                                                                               \
+    memset(list->data + list->length, 0,                                       \
+           (capacity - list->length) * sizeof(TYPE));                          \
+  }                                                                            \
+                                                                               \
+  void list_##TYPE##_push(list_##TYPE##_t *list, TYPE item) {                  \
+    if (list->capacity == 0) {                                                 \
+      list_##TYPE##_set_capacity(list, 1);                                     \
+    } else if (list->length == list->capacity) {                               \
+      list_##TYPE##_set_capacity(list, list->capacity * 2);                    \
+    }                                                                          \
+                                                                               \
+    list->data[list->length++] = item;                                         \
+  }                                                                            \
+                                                                               \
+  void list_##TYPE##_free(list_##TYPE##_t *list) {                             \
+    assert(list->data != nullptr);                                             \
+                                                                               \
+    free((void *)list->data);                                                  \
+    list->length = 0;                                                          \
+    list->capacity = 0;                                                        \
+  }                                                                            \
+                                                                               \
+  void list_##TYPE##_pop(list_##TYPE##_t *list) {                              \
+    assert(list->length > 0);                                                  \
+                                                                               \
+    list->data[--list->length] = (TYPE){};                                     \
+                                                                               \
+    if (list->length < list->capacity / 4 && list->length > 4) {               \
+      list_##TYPE##_set_capacity(list, list->length * 2);                      \
+    }                                                                          \
+  }
+
+DEFINE_ARRAY(int)
+DEFINE_ARRAY(float)
