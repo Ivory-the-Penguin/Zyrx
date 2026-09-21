3c9ec4216a9e3426e0c0476deb5c77101ad15290
Ivory <ivory-the-penguin@proton.me>
2026-09-21 19:25:12 +0100

    Hashmap make function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "build.h"
 #include "core/list.h"
 
 #define HASHMAP_SMALL_PRIME 53ull
+#define HASHMAP_MINIMUM_CAPACITY 16
 
 static inline uint64_t hashmap_make_hash(const char* string, uint64_t size) {
   uint64_t hash_value = 0, small_prime_pow = 1;
@@ -28,5 +30,12 @@ typedef struct {
   hashmap_int_slot_t* slots;
   uint64_t capacity;
 } hasmap_int_t;
+
+static inline hasmap_int_t hashmap_int_make() {
+  return (hasmap_int_t){
+      .slots = calloc(HASHMAP_MINIMUM_CAPACITY, sizeof(hashmap_int_slot_t)),
+      .capacity = HASHMAP_MINIMUM_CAPACITY,
+  };
+}
 
 int main() {}
